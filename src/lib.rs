fece954fcfdba24e6e53b894837a0119343611e7
Jan Tojnar <jtojnar@gmail.com>
2025-06-10 13:33:48 +0200

    fixup! ser: Write element for struct unless transparent

--- src/lib.rs
+++ src/lib.rs
@@ -87,6 +87,7 @@ mod tests {
 
     #[test]
     fn encode() {
+        pretty_env_logger::init();
         assert_eq!(
             r#"<?xml version="1.0" encoding="UTF-8"?>
 <epp xmlns="urn:ietf:params:xml:ns:epp-1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd">
